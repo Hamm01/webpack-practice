57dbf5eda29f46f695768b57189314b3795de18d
Himanish kochhar <himanish.kochhar@gmail.com>
2022-06-02 15:12:41 +0530

    Bable packages and loader configured in webpack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import getclasses from "./getClasses"
 console.log("Ran from the index.js")
 getclasses()
+
+const obj = { a: "alpha", b: "bravo" }
+const newObj = { ...obj, c: "charlie" }
+console.log(newObj)
